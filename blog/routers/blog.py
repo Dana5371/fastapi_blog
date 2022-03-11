0f3cfa57330f84f4925a833ebb54cf58abac1b23
Dana5371 <danaker.qw@gmail.com>
2022-03-11 15:12:20 +0600

    router, repository

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,19 +1,38 @@
-from fastapi import APIRouter, status, Depends
-import schemas, database, models
+import re
+from fastapi import APIRouter, HTTPException, Response, status, Depends
+import schemas,models
 from database import SessionLocal
 from sqlalchemy.orm import Session
-
-router = APIRouter()
-
-
-
+from typing import List
+from database import get_db
+from repository import blog
 
 
+router = APIRouter(
+    tags=['blogs']
+)
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED, tags=['blogs'])
-def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id = 1  )
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+
+@router.post("/blog", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.create(request, db)
+
+
+@router.get('/blog', response_model=List[schemas.ShowBlog], tags=['blogs'])
+def all(db: Session = Depends(get_db)):
+    return blog.get_all(db)
+
+
+@router.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    return blog.delete(id, db)
+
+
+@router.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.update(id, request, db)
+
+
+@router.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+def show(id: int, response: Response, db: Session = Depends(get_db)):
+    return blog.get_one(id, db)
---
+++ blog/routers/user.py
@@ -0,0 +1,19 @@
+import schemas, models
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from repository import user
+
+
+router = APIRouter(
+    tags=['users']
+)
+
+@router.post('/user', response_model=schemas.ShowUser)
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    return user.create_user(request, db)
+
+
+@router.get('/user/{id}', response_model=schemas.ShowUser)
+def get_user(id, db: Session = Depends(get_db)):
+    return user.get_user(id, db)
